25a0e68406b53f434cb33aeeef725809aa9ba2e0
sodate-tf <ftsodate@gmail.com>
2025-08-16 20:58:00 -0300

    versao 1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en">      
+        <head>
+          <title>Pergunte ao Tio Ben - Respostas Católicas com IA</title>
+          <meta name="description" content="Faça perguntas ao Tio Ben, nosso catequista virtual, e receba respostas baseadas na Bíblia, Catecismo e tradição da Igreja Católica. Aprenda e aprofunde sua fé com simplicidade e clareza." />
+          <meta name="keywords" content="Tio Ben, perguntas católicas, catequista virtual, respostas católicas, fé, Bíblia Católica, Catecismo, Igreja Católica, estudo da fé, aprendizado religioso, app de perguntas, IA católica" />
+          <meta name="author" content="4U Develops" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="canonical" href="https://www.iatioben.com.br" />
+
+          <meta property="og:title" content="Pergunte ao Tio Ben - Catequista Virtual Católico" />
+          <meta property="og:description" content="Faça suas perguntas ao Tio Ben e receba respostas católicas confiáveis e claras. Baseadas na Bíblia, Catecismo e tradição da Igreja." />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.iatioben.com.br" />
+          <meta property="og:image" content="https://www.iatioben.com.br/images/og-image.png" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:site_name" content="Pergunte ao Tio Ben" />
+
+          <meta name="google-adsense-account" content="ca-pub-8819996017476509"></meta>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
