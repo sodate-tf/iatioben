42b6f55f1c650eb4f6717745b994a1061b8d70ba
sodate-tf <ftsodate@gmail.com>
2025-09-07 19:09:32 -0300

    versao 4.6

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-           <link rel="apple-touch-icon" href="/tio-ben-180x180.png" />
+        <link rel="apple-touch-icon" href="/tio-ben-180x180.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
