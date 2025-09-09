514423594c883e4810c97acfcf3b1be1c78053bb
sodate-tf <ftsodate@gmail.com>
2025-09-09 09:10:23 -0300

    versao 4.13

--- components/testBanner.tsx
+++ components/testBanner.tsx
@@ -5,16 +5,29 @@ import Image from "next/image";
 import Link from "next/link";
 export default function TestFooterBanner() {
   return (
+    <>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8819996017476509"
+     crossOrigin="anonymous"></script>
+
+
+    
     <div className="absolut bottom-0 left-0 w-full flex justify-center items-center bg-gray-200 border-t border-gray-400 z-50 p-2">
       {/* Banner responsivo */}
       <div className="bg-gray-400 text-white font-bold flex justify-center items-center"
            style={{
              width: "100%",            
            }}>
-        <p className="text-center">
-          <div id="ezoic-pub-ad-placeholder-111"></div>
-        </p>
+        <ins className="adsbygoogle display:block"
+            data-ad-client="ca-pub-8819996017476509"
+            data-ad-slot="9591116531"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    
       </div>
     </div>
+    </>
   );
 }
