e1b5c939cbf192b9cda2e91c8c7639846217644a
sodate-tf <ftsodate@gmail.com>
2025-09-25 12:34:21 -0300

    5.15

--- app/liturgia-diaria/page.tsx
+++ app/liturgia-diaria/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = createMetaData({
   title: 'Liturgia Diária - Tio Ben',
   description: 'Acompanhe a liturgia diária e mantenha-se em unidade com a Igreja, meditando as mesmas leituras proclamadas em todo o mundo.',
   url: 'https://www.iatioben.com.br/liturgia-diaria',
-  image: 'https://www.iatioben.com.br/images/og_image_liturgia.png'
+  image: 'https://www.iatioben.com.br/og_image_liturgia.png'
 });
 
 export default function Page() {
