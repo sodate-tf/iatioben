79d696e8c787517c7adfe010174224df7b9de44d
sodate-tf <ftsodate@gmail.com>
2025-09-25 12:01:52 -0300

    5.7

--- app/liturgia-diaria/page.tsx
+++ app/liturgia-diaria/page.tsx
@@ -234,10 +234,7 @@ const LiturgiaDiaria: React.FC = () => {
     <>
      <Head>
         <title>Liturgia Diária - Tio Ben</title>
-        <meta
-          name="description"
-          content="Acompanhe a liturgia diária e mantenha-se em unidade com a Igreja, meditando as mesmas leituras proclamadas em todo o mundo."
-        />
+        <meta name="description" content="Acompanhe a liturgia diária e mantenha-se em unidade com a Igreja, meditando as mesmas leituras proclamadas em todo o mundo." />
         <meta property="og:title" content="Liturgia Diária - Tio Ben" />
         <meta
           property="og:description"
@@ -245,7 +242,7 @@ const LiturgiaDiaria: React.FC = () => {
         />
         <meta property="og:image" content="/og_image_liturgia.png" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://seudominio.com/liturgia-diaria" />
+        <meta property="og:url" content="https://www.iatioben.com.br/liturgia-diaria" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Liturgia Diária - Tio Ben" />
         <meta
